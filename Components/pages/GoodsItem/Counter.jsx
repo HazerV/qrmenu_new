a6c3b49087902e4d52b3b9623a6e759fbd55c39e
HazerV <krotkov2004@icloud.com>
2023-10-14 02:48:07 +0400

    wtrhCOunt

--- Components/pages/GoodsItem/Counter.jsx
+++ Components/pages/GoodsItem/Counter.jsx
@@ -3,45 +3,73 @@ import {View, StyleSheet, Text} from 'react-native';
 import PlusSvg from '../GoodsItem/Plus.svg'
 import MinusSvg from '../GoodsItem/Minus.svg'
 
+
 const Counter = () => {
     return (
-        <View style={{ 
-            width: 143,
-            backgroundColor: 'gray',
-            borderRadius: 16,
-            marginLeft: 16,
-            marginTop: 12,
-            height: 40,
-            marginBottom: 16,
-            flexShrink: 1,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            padding: 8
-            }}>
+        <View style={styles.container}>
+             <View style={styles.mainPM}>
+                     <View>
+                         <MinusSvg />
+                     </View>
+             
+                     <Text style = {styles.text}>
+                         455 руб 
+                     </Text>
+                 
+                     <View>
+                         <PlusSvg />
+                     </View>
+                 
+             </View>
+
+             <View style={styles.counter} >
                 
-
-                <View>
-                    <MinusSvg />
-                </View>
-
-                <Text style = {{
-                    color: 'white',
-                    fontFamily: 'Gilroy-Regular',
-                    fontSize: 16,
-                    fontWeight: 400,
-                    alignSelf: 'center'
-                }}>
-                    423 руб
-                </Text>
-
-                <View>
-                    <PlusSvg />
-                </View>
+             </View>
         </View>
     );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        columnGap: 8
+    },
+    mainPM: { 
+        width: 143,
+        backgroundColor: 'gray',
+        borderRadius: 16,
+        marginLeft: 16,
+        marginTop: 12,
+        height: 40,
+        marginBottom: 16,
+        flexShrink: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 8
+    },
+    text: {
+        color: 'white',
+        fontFamily: 'Gilroy-Regular',
+        fontSize: 16,
+        // fontWeight: 400,
+        alignSelf: 'center'
+    },
+    counter: {
+        width: 38,
+        backgroundColor: 'gray',
+        borderRadius: 16,
+        marginLeft: 16,
+        marginTop: 12,
+        height: 40,
+        marginBottom: 16,
+        flexShrink: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 8
+}
+})
 
 export default Counter;
